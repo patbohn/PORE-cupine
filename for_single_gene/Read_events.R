9c7958275e1ae400b1fb216ff48bf23f2e95914f
Patrick Bohn <62295512+patbohn@users.noreply.github.com>
2022-09-07 11:10:53 +0200

    Update Read_events.R

--- for_single_gene/Read_events.R
+++ for_single_gene/Read_events.R
@@ -36,12 +36,12 @@ getCurrentFileLocation <-  function()
     {
       this_file <- rstudioapi::getSourceEditorContext()$path
     }
-    return(trimws(dirname(this_file)))
+    return(dirname(this_file))
 }
 
 
 #script.dir <- dirname(sys.frame(1)$ofile)
-Rcpp::sourceCpp(paste(getCurrentFileLocation(), "/for_r.cpp"))
+Rcpp::sourceCpp(trimws(paste(getCurrentFileLocation(), "/for_r.cpp")))
 
 #loading of event files
 dat= fread(paste(opt$file))
